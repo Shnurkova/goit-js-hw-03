3e97fc85aecf8f2f4a5cd65450355cbee64ea057
Shnurkova <Annshnur@gmail.com>
2024-01-26 16:12:21 +0200

    add task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,4 @@
-function makeArray([firstArray], [secondArray], maxLength) {
+function makeArray(firstArray, secondArray, maxLength) {
     const newArray = firstArray.concat(secondArray);
     if (newArray.length > maxLength) {
         return newArray.slice(0, maxLength);
